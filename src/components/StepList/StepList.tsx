c46fad214e44a3beb4bd738d519ac8c5bddcbb7a
Ziwei Liu <>
2019-10-05 12:57:36 +1300

    feat: alert component for feedback after you complete a cc

--- src/components/StepList/StepList.tsx
+++ src/components/StepList/StepList.tsx
@@ -1,6 +1,8 @@
 import { Container } from '@material-ui/core';
+import { useRouter } from 'next/router';
 import React, { FC, Fragment } from 'react';
 import { stepListQuery_steps_nodes as stepListQueryStepsNodes } from '../../containers/StepList/types/stepListQuery';
+import Alert from './Alert/Alert';
 import Item from './Item';
 
 interface Props {
@@ -8,21 +10,31 @@ interface Props {
   items?: stepListQueryStepsNodes[];
 }
 
-const StepList: FC<Props> = ({ loading, items }) => (
-  <Container>
-    {loading ? (
-      <Item loading={true}></Item>
-    ) : (
-      <Fragment>
-        {items &&
-          items.map(step => (
-            <Fragment key={step.id}>
-              <Item step={step} />
-            </Fragment>
-          ))}
-      </Fragment>
-    )}
-  </Container>
-);
+const StepList: FC<Props> = ({ loading, items }) => {
+  const router = useRouter();
+
+  return (
+    <Container>
+      {router.query.connectCard && (
+        <Alert
+          initialOpen={router.query.connectCard.valueOf() === 'true'}
+          alertMessage={'Thanks for filling out your connect card'}
+        />
+      )}
+      {loading ? (
+        <Item loading={true}></Item>
+      ) : (
+        <Fragment>
+          {items &&
+            items.map(step => (
+              <Fragment key={step.id}>
+                <Item step={step} />
+              </Fragment>
+            ))}
+        </Fragment>
+      )}
+    </Container>
+  );
+};
 
 export default StepList;
